638c28c7da1140f36d365c45f2769a8ff291e8e9
Augusto Pin <augustopm607@gmail.com>
2025-05-14 17:55:57 -0500

    Initial commit

---
+++ src/main/java/com/car2go/car2go_iam_service/Car2goIamServiceApplication.java
@@ -0,0 +1,13 @@
+package com.car2go.car2go_iam_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Car2goIamServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Car2goIamServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/car2go/car2go_iam_service/Car2goIamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car2go.car2go_iam_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Car2goIamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
