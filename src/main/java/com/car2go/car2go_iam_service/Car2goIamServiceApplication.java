db56a587e48dd02b91c088858ec30f5e87aadcea
Franco1625 <u202219284@upc.edu.pe>
2025-05-16 17:45:21 -0500

    fix(iam): enable JPA auditing to fix created_at not null error

--- src/main/java/com/car2go/car2go_iam_service/Car2goIamServiceApplication.java
+++ src/main/java/com/car2go/car2go_iam_service/Car2goIamServiceApplication.java
@@ -2,7 +2,19 @@ package com.car2go.car2go_iam_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * Car2goIamServiceApplication
+ *
+ * @summary
+ * The main class of the Car2go IAM Service application.
+ * It is responsible for starting the Spring Boot application.
+ * It also enables JPA auditing.
+ *
+ * @since 1.0
+ */
+@EnableJpaAuditing
 @SpringBootApplication
 public class Car2goIamServiceApplication {
 
